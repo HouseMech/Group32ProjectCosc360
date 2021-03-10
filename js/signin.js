fea3c9b821ad8f589a2d1051b14bc9ca492d06d8
Pamal Mangat <mangatpamal@gmail.com>
2021-03-10 15:32:14 -0800

    /Home, /SignUp, /SignIn
    - Added Layout for /Home Page, /Signup, /Signin.
    
    - Added css transition and styling effects for those 3 pages also.
    
    - Linked js files and Jquery to signin.html file.
    
    TODO NEXT:
    - Link Database to Project
    - Finish  backend Javascript validation for signup and sign in forms & test it against the database.
    Then we can continue working on the remainder of the pages (since the remainder of the pages are dynamic).

---
+++ js/signin.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  // Empty
+});
+
+function signUp_Visit() {
+  // Redirect user to signUp page after clicking 'Sign Up' button.
+  window.location.href = "../pages/signUp.html";
+}
