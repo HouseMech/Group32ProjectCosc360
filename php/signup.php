d80eaf761753ade3b5e28b1171f02703720c9433
Rileyccc <50608798+Rileyccc@users.noreply.github.com>
2021-03-12 18:27:47 -0800

    Revert "Revert "make async request to sign in/error handling and page redirect""
    This reverts commit c5591047afdaa2e7d0f5a2c0899d61daf2d02539.

--- php/signup.php
+++ php/signup.php
@@ -1,18 +1,21 @@
 <?php
 // add new user to database unless someone else has the same username or email. 
 // if account registration correct set session varable 'login' to true
-session_start();
 // credentials 
 $user = 'root';
 $pass = '';
 $dbname = 'blog';
 
 // start session so if the user is logged in can be tracked across pages
-session_start();
+// session_status means there is no session started
+if(session_status() != PHP_SESSION_ACTIVE ){
+    session_start();
+}
 // if user already loggeed in exit
-if(!is_null($Session['login'] && $_SESSION['login'] == true)){
+if(isset($_SESSION['login']) && $_SESSION['login'] == true){
     exit("You are already logged in!");
 }
+
 
 // get info from post
 $userName = $_POST["userName"];
@@ -28,7 +31,7 @@ $hash = password_hash($password,
 // create connection
 $conn = new mysqli('localhost', $user, $pass, $dbname) or die("unable to connect");
 // check if anyone has same email reject request
-$stmt = $conn->prepare("SELECT email FROM user WHERE email = ?");
+$stmt = $conn->prepare("SELECT email FROM blogUser WHERE email = ?");
 $stmt->bind_param("s", $email);
 $stmt->execute();
 $result = $stmt->get_result();
@@ -37,7 +40,7 @@ if(!is_null($result->fetch_assoc())){
     exit("Email is already taken");
 }
 $stmt->close();
-$stmt = $conn->prepare("SELECT userName FROM user WHERE userName = ?");
+$stmt = $conn->prepare("SELECT userName FROM blogUser WHERE userName = ?");
 $stmt->bind_param("s", $userName);
 $stmt->execute();
 $result = $stmt->get_result();
@@ -46,11 +49,11 @@ if(!is_null($result->fetch_assoc())){
     exit("userName is taken");
 }
 $stmt->close();
-$stmt = $conn->prepare("INSERT INTO user (userName, password, firstName, lastName, email ) VALUES(?, ?, ?, ?, ?)");
+$stmt = $conn->prepare("INSERT INTO blogUser (userName, password, firstName, lastName, email ) VALUES(?, ?, ?, ?, ?)");
 $stmt->bind_param("sssss", $userName, $hash , $fName, $lName, $email,);
 if($stmt->execute()){
     $_SESSION["login"] = true;
-    exit("Success! welcome to BLOG!");
+    exit("success");
 }else{
     exit("Something went wrong at our end try again");
 }
