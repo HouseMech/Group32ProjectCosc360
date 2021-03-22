9c59271eeeae98d4e04f551db78105b7461a6611
Pamal Mangat <mangatpamal@gmail.com>
2021-03-22 13:08:41 -0700

    newPost.php finished.
    - Slightly modified post table database structure. Added comment column, and changed default values for image and image path if no image is provided by user on the blog post. Also added new column to hold the image path that will be saved as 'postid (pid)'.jpg. Each post will have it's own jpeg if provided by the user.
    
    - newPost.php now is complete, and redirects user back to profile page where they can view their updated post on their profile

--- php/newPost.php
+++ php/newPost.php
@@ -9,10 +9,24 @@
   $desc = $_POST['pDesc']; // required
   $tags = $_POST['pTags']; // optional
 
-  // Prepare/save image for upload to db storage.
-  $filename = $_FILES["pImg"]["name"]; 
-  $tempname = $_FILES["pImg"]["tmp_name"];
+  global $tempname;
+  global $image;
 
+  // Prepare/save image for upload to db storage (BLOB, etc).
+  $filename = $_FILES['pImg']['name'];
+  if (empty($_FILES['pImg']['tmp_name']) != true){
+    $image = base64_encode(file_get_contents(addslashes($_FILES['pImg']['tmp_name'])));
+
+     // Update the image to be saved as the pid.jpg
+    $conn = new mysqli('localhost', $user, $pass, $dbname) or die("unable to connect");
+    $sql = "SELECT MAX(pid) FROM post";
+    // Get pid for this post, increment by one because it hasn't been added into db yet.
+    if ($row = $conn -> query($sql)) {$pid = $row -> fetch_row()[0] + 1;}
+    $tempname = $pid . '.jpg';
+  } else {
+    $image = NULL;
+    $tempname = NULL;
+  }
 
   // Determine if commenting is turned on/off. 
   $allowComments = 1;
@@ -22,21 +36,17 @@
     $allowComments = 0;
   }
 
-  echo 'Post Title: <b>' . $title . '</b><br>';
-  echo 'Post Description: <b>' . $desc . '</b><br>';
-  echo 'Post Tags: <b>' . $tags . '</b><br>';
-  echo 'Post Image: <b>' . $filename . '</b><br>';
-  echo 'Post Commenting: <b>' . $allowComments . '</b><br>';
-  echo $_SESSION['username'] . '<br/>';
-
   // create connection and insert post into database.
-  /*
   $conn = new mysqli('localhost', $user, $pass, $dbname) or die("unable to connect");
-  $stmt = $conn->prepare("INSERT INTO post (pUserName, description, image, likes, postname, topic) VALUES(?, ?, ?, ?, ?, ?)");
+  $stmt = $conn->prepare("INSERT INTO post (pUserName, description, imagePath, image, likes, postname, topic, allowComment) VALUES(?, ?, ?, ?, ?, ?, ?, ?)");
   $likes = 0;
-  $stmt->bind_param("ssssss", $_SESSION['username'], $desc , $image, $likes, $title, $tags);
-  $stmt->execute();
-  $stmt->close();
-  */
+  $stmt->bind_param("ssssssss", $_SESSION['username'], $desc , $tempname, $image, $likes, $title, $tags, $allowComments);
+  if($stmt->execute()){
+    $stmt->close();
+    header("Location: ../pages/profile.html");
+    exit("success");
+  } else{
+    exit("Could not submit post. Please try again!");
+  }
   
 ?>
