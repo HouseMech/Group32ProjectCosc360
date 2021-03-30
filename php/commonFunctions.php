a0c40551399be82a527802610944b5f91dd6fa2b
Rileyccc <50608798+Rileyccc@users.noreply.github.com>
2021-03-30 15:08:10 -0700

    make file for common functions

---
+++ php/commonFunctions.php
@@ -0,0 +1,24 @@
+<?php
+// create connection funcion
+function createConnection(){
+    // credentials 
+    $user = 'root';
+    $pass = '';
+    $dbname = 'blog';
+    $conn = new mysqli('localhost', $user, $pass, $dbname) or die("unable to connect");
+    return $conn;
+}
+// if no session running starts one
+function startSession(){
+    if(session_status() != PHP_SESSION_ACTIVE ){
+        session_start();
+    }
+}
+// checks if user is logged In
+function isLoggedIn(){
+    if(isset($_SESSION['login']) && $_SESSION['login'] == true){
+        return true;
+    }
+    return false;
+}
+?>
