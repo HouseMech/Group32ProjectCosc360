f5926abcdefc05253cdd7559132521bf5559b050
Liam Harbec <liam.harbec@gmail.com>
2021-03-31 10:25:25 -0700

    Added ability to see username in sidebar

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -1,8 +1,21 @@
 <!-- Display sidebar depending on user login status. (Show if logged in, hide if not)-->
+<?php
+  session_start();
+?>
 <div id="sidebar">
   <div id="sidebar-topdiv">
     <img id="userProfilePicture" src="https://painrehabproducts.com/wp-content/uploads/2014/10/facebook-default-no-profile-pic.jpg" alt="userProfilePicture">
-    <p id="sidebar-username">Username</p>
+    <p id="sidebar-username">
+      <?php
+      if ((isset($_SESSION['login'])) && ($_SESSION['login'] == true)) {
+          echo $_SESSION['username'];
+      }
+      else {
+        echo 'username';
+      }
+    ?>
+    </p>
+
   </div>
   <div id="sidebar-bottomdiv">
     <div id="sidebar-btn" onclick="location.href='pages/newPost.php'">New Post</div>
