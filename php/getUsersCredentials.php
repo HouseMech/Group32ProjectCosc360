0f3a4c2d470086f18bdd89f929261bae42394a31
Rileyccc <50608798+Rileyccc@users.noreply.github.com>
2021-04-01 13:15:46 -0700

    Create getUsersCredentials.php

---
+++ php/getUsersCredentials.php
@@ -0,0 +1,18 @@
+<?php
+# Current assumption sidebar is hidden so only logged in users can see account settings
+# so will not check if they are logged in might have to change later..
+include "commonFunctions.php";
+
+startSession();
+
+$conn = createConnection();
+
+$username = $_SESSION['username'];
+# not using a prepared statement because username has already been validated 
+$result = $conn->query("SELECT * From bloguser WHERE userName='$username'");
+# since no one can share a username and this user is currently logged in
+# based off assumption there must be an output
+$row = $result->fetch_assoc();
+# return json output
+echo json_encode($row);
+?>
