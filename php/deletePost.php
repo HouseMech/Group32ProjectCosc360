35ea4f9cf0c2f074108144b9007153d1397a8c2b
Pamal Mangat <mangatpamal@gmail.com>
2021-04-01 15:25:46 -0700

    Modified deletePost.php
    - /deleteAccount.php had to be re-factors to delete from comment table first than post table (due to foreign key constraint)

--- php/deletePost.php
+++ php/deletePost.php
@@ -4,6 +4,12 @@
   $pid = $_GET['pid'];
   // create connection and delete post (pid) from posts table.
   $conn = createConnection();
+  // First delete all comments linked to that post. (Foreign key constraint)
+  $stmt = $conn->prepare("DELETE FROM comment WHERE pid = ?");
+  $stmt->bind_param("s", $pid);
+  $stmt->execute();
+  $stmt->close();
+  
   $stmt = $conn->prepare("DELETE FROM post WHERE pid = ?");
   $stmt->bind_param("s", $pid);
   $stmt->execute();
