fbbeb3670bac08afd646ba129d6a38d89e827959
Liam Harbec <liam.harbec@gmail.com>
2021-04-01 18:05:17 -0700

    Removed user sidebar if user is not logged in

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -2,25 +2,22 @@
 <?php
   session_start();
 ?>
-<div id="sidebar">
-  <div id="sidebar-topdiv">
-    <img id="userProfilePicture" src="https://painrehabproducts.com/wp-content/uploads/2014/10/facebook-default-no-profile-pic.jpg" alt="userProfilePicture">
-    <p id="sidebar-username">
-      <?php
-      if ((isset($_SESSION['login'])) && ($_SESSION['login'] == true)) {
+<?php if ((isset($_SESSION['login'])) && ($_SESSION['login'] == true)) : ?>
+  <div id="sidebar">
+    <div id="sidebar-topdiv">
+      <img id="userProfilePicture" src="https://painrehabproducts.com/wp-content/uploads/2014/10/facebook-default-no-profile-pic.jpg" alt="userProfilePicture">
+      <p id="sidebar-username">
+        <?php
           echo $_SESSION['username'];
-      }
-      else {
-        echo 'username';
-      }
-    ?>
-    </p>
+      ?>
+      </p>
 
+    </div>
+    <div id="sidebar-bottomdiv">
+      <div id="sidebar-btn" onclick="location.href='pages/newPost.php'">New Post</div>
+      <div id="sidebar-btn" onclick="location.href='php/getPosts.php'">View Posts</div>
+      <div id="sidebar-btn" onclick="location.href='pages/viewComments.php'">View Comments</div>
+      <div id="sidebar-btn" onclick="location.href='pages/accountSettings.php'">Account Settings</div>
+    </div>
   </div>
-  <div id="sidebar-bottomdiv">
-    <div id="sidebar-btn" onclick="location.href='pages/newPost.php'">New Post</div>
-    <div id="sidebar-btn" onclick="location.href='php/getPosts.php'">View Posts</div>
-    <div id="sidebar-btn" onclick="location.href='pages/viewComments.php'">View Comments</div>
-    <div id="sidebar-btn" onclick="location.href='pages/accountSettings.php'">Account Settings</div>
-  </div>
-</div>
+<?php endif;  ?>
