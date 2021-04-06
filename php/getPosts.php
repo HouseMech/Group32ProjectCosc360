ee34601e3fd7512ad362147282f3b8ae104069c9
Pamal Mangat <mangatpamal@gmail.com>
2021-04-06 14:29:24 -0700

    Increased Security on each page and user login detection
    - Increased security added to each page.
    - User won't be able to view the pages that require signin. For example, the pages such as New Post, View Posts, View Comments, View Account Settings all display an error message to the user directing them to sign in before accessing that specific page.

--- php/getPosts.php
+++ php/getPosts.php
@@ -10,95 +10,101 @@
       <h2 id='subHead'>View Your Posts:</h2>
       <?php
         startSession();
-        $username = $_SESSION['username'];
+        // Prevent user from accessing this page unless signed in.
+        if (empty($_SESSION["username"])){$username = 'NULL';} else {$username = $_SESSION['username'];}
+        if ($username != 'NULL'){
+          // Display post with the newest post first.
+          $conn = createConnection();
+          $stmt = $conn->prepare("SELECT * FROM post WHERE pUserName = ? ORDER BY time DESC");
+          $stmt->bind_param("s", $username);
+          $stmt->execute();
+          $result = $stmt->get_result();
+          $num_posts = $result -> num_rows;
+          if ($num_posts == 1){
+            echo "<div id='numPosts'>";
+            echo '<h2>You have <u>' . $num_posts . '</u> post!</h2>';
+            echo "</div>";
+          } else {
+            echo "<div id='numPosts'>";
+            echo '<h2>You have <u>' . $num_posts . '</u> posts!</h2>';
+            echo "</div>";
+          }
+          if ($result){
+            // Append this attributes, one div for each post.
+            while ($row = $result->fetch_assoc()){
+              // Fetch attributes for each post by each this user.
+              $pid = $row['pid'];
+              $pUserName = $row['pUserName'];
+              $description = $row['description'];
+              $time = $row['time'];
+              $imagePath = $row['imagePath'];
+              $likes = $row['likes'];
+              $postTitle = $row['postName'];
+              $postTopic = $row['topic'];
+              $allowComments = $row['allowComment'];
 
-        $conn = createConnection();
-        // Display post with the newest post first.
-        $stmt = $conn->prepare("SELECT * FROM post WHERE pUserName = ? ORDER BY time DESC");
-        $stmt->bind_param("s", $username);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $num_posts = $result -> num_rows;
-        if ($num_posts == 1){
-          echo "<div id='numPosts'>";
-          echo '<h2>You have <u>' . $num_posts . '</u> post!</h2>';
-          echo "</div>";
+              // Display post title.
+              echo "<div id='blogPost'>";
+              echo "<h2>" . $postTitle . "</h2>";
+
+              // Display post time.
+              echo "<p id='time-log'>" . $time . "</p>";
+
+              // Display description.
+              echo "<div id='desc-log'>";
+              echo "<h5>" . $description . "</h5>";
+              echo "</div>";
+
+              // Link to open attached image in new tab.
+              if (!empty($imagePath)) {
+                echo "<div id='img-link'><a href=" . $imagePath . ">View Attached Image</a></div>";
+              }
+
+              // Display post topic, author, and # likes.
+              echo "<div id='post-log'>";
+              echo "<table id='post-table'>";
+              echo "<tr>";
+              echo "<th id='post-header'>Post Topic:</th>";
+              echo "<th id='post-header'>Author:</th>";
+              echo "<th id='post-header'># Likes:</th>";
+              echo "</tr>";
+              
+              echo "<tr>";
+              echo "<td>";
+              if (empty($postTopic)){
+                echo "<h3 id='post-item'>" . 'None' . "</u></h3>";
+              } else {
+                echo "<h3 id='post-item'>" . $postTopic . "</u></h3>";
+              }
+              echo "</td>";
+              echo "<td>" . "<h3 id='post-item'>" . $pUserName . "</h3></td>";
+              echo "<td>" . "<h3 id='post-item'>" . '(' . $likes . ') 👍' . "</h3></td>";
+              echo "</tr>";
+              echo "</table>";
+              echo "</div>";
+
+              echo "<div id='btn-holder'>";
+
+              // View post button.
+              echo "<form id='btn-item' action='php/post.php?pid=" . $pid . "' method='get'>";
+              echo "<button id='btn-view' type='submit' formmethod='post'>View Post</button>";
+              echo "</form>";
+
+              // Delete post button.
+              echo "<form id='btn-item' action='php/deletePost.php?pid=" . $pid . "' method='get'>";
+              echo "<button id='btn-delete' type='submit' formmethod='post'>Delete Post</button>";
+              echo "</form>";
+
+              echo "</div>";
+              echo "</div>";
+              echo "<p id='spacer'>____________________________________</p>";
+            }
+          }
         } else {
           echo "<div id='numPosts'>";
-          echo '<h2>You have <u>' . $num_posts . '</u> posts!</h2>';
+          echo '<h2>You must be signed in to view this page.</h2>';
           echo "</div>";
         }
-        if ($result){
-          // Append this attributes, one div for each post.
-          while ($row = $result->fetch_assoc()){
-            // Fetch attributes for each post by each this user.
-            $pid = $row['pid'];
-            $pUserName = $row['pUserName'];
-            $description = $row['description'];
-            $time = $row['time'];
-            $imagePath = $row['imagePath'];
-            $likes = $row['likes'];
-            $postTitle = $row['postName'];
-            $postTopic = $row['topic'];
-            $allowComments = $row['allowComment'];
-
-            // Display post title.
-            echo "<div id='blogPost'>";
-            echo "<h2>" . $postTitle . "</h2>";
-
-            // Display post time.
-            echo "<p id='time-log'>" . $time . "</p>";
-
-            // Display description.
-            echo "<div id='desc-log'>";
-            echo "<h5>" . $description . "</h5>";
-            echo "</div>";
-
-            // Link to open attached image in new tab.
-            if (!empty($imagePath)) {
-              echo "<div id='img-link'><a href=" . $imagePath . ">View Attached Image</a></div>";
-            }
-
-            // Display post topic, author, and # likes.
-            echo "<div id='post-log'>";
-            echo "<table id='post-table'>";
-            echo "<tr>";
-            echo "<th id='post-header'>Post Topic:</th>";
-            echo "<th id='post-header'>Author:</th>";
-            echo "<th id='post-header'># Likes:</th>";
-            echo "</tr>";
-            
-            echo "<tr>";
-            echo "<td>";
-            if (empty($postTopic)){
-              echo "<h3 id='post-item'>" . 'None' . "</u></h3>";
-            } else {
-              echo "<h3 id='post-item'>" . $postTopic . "</u></h3>";
-            }
-            echo "</td>";
-            echo "<td>" . "<h3 id='post-item'>" . $pUserName . "</h3></td>";
-            echo "<td>" . "<h3 id='post-item'>" . '(' . $likes . ') 👍' . "</h3></td>";
-            echo "</tr>";
-            echo "</table>";
-            echo "</div>";
-
-            echo "<div id='btn-holder'>";
-
-            // View post button.
-            echo "<form id='btn-item' action='php/post.php?pid=" . $pid . "' method='get'>";
-            echo "<button id='btn-view' type='submit' formmethod='post'>View Post</button>";
-            echo "</form>";
-
-            // Delete post button.
-            echo "<form id='btn-item' action='php/deletePost.php?pid=" . $pid . "' method='get'>";
-            echo "<button id='btn-delete' type='submit' formmethod='post'>Delete Post</button>";
-            echo "</form>";
-
-            echo "</div>";
-            echo "</div>";
-            echo "<p id='spacer'>____________________________________</p>";
-        }
-      }
       ?>
     </div>
   </div>
--- php/search.php
+++ php/search.php
@@ -20,7 +20,83 @@
         $result = $stmt->get_result();
         $num_posts = $result -> num_rows;
         echo "<div id='numPosts'>";
-        echo '<h2><u>' . $num_posts . '</u> Post Result(s).</h2>';
+        echo '<h2><u>' . $num_posts . '</u> Post Title Result(s).</h2>';
+        echo "</div>";
+
+        if ($result){
+          // Append this attributes, one div for each post.
+          while ($row = $result->fetch_assoc()){
+            // Fetch attributes for each post by each this user.
+            $pid = $row['pid'];
+            $pUserName = $row['pUserName'];
+            $description = $row['description'];
+            $time = $row['time'];
+            $imagePath = $row['imagePath'];
+            $likes = $row['likes'];
+            $postTitle = $row['postName'];
+            $postTopic = $row['topic'];
+            $allowComments = $row['allowComment'];
+
+            // Display post title.
+            echo "<div id='blogPost'>";
+            echo "<h2>" . $postTitle . "</h2>";
+
+            // Display post time.
+            echo "<p id='time-log'>" . $time . "</p>";
+
+            // Display description.
+            echo "<div id='desc-log'>";
+            echo "<h5>" . $description . "</h5>";
+            echo "</div>";
+
+            // Link to open attached image in new tab.
+            if (!empty($imagePath)) {
+              echo "<div id='img-link'><a href=" . $imagePath . ">View Attached Image</a></div>";
+            }
+
+            // Display post topic, author, and # likes.
+            echo "<div id='post-log'>";
+            echo "<table id='post-table'>";
+            echo "<tr>";
+            echo "<th id='post-header'>Post Topic:</th>";
+            echo "<th id='post-header'>Author:</th>";
+            echo "<th id='post-header'># Likes:</th>";
+            echo "</tr>";
+            
+            echo "<tr>";
+            echo "<td>";
+            if (empty($postTopic)){
+              echo "<h3 id='post-item'>" . 'None' . "</u></h3>";
+            } else {
+              echo "<h3 id='post-item'>" . $postTopic . "</u></h3>";
+            }
+            echo "</td>";
+            echo "<td>" . "<h3 id='post-item'>" . $pUserName . "</h3></td>";
+            echo "<td>" . "<h3 id='post-item'>" . '(' . $likes . ') 👍' . "</h3></td>";
+            echo "</tr>";
+            echo "</table>";
+            echo "</div>";
+            
+            // View post button.
+            echo "<div id='btn-holder'>";
+            echo "<form id='btn-item' action='php/post.php?pid=" . $pid . "' method='get'>";
+            echo "<button id='btn-view' type='submit' formmethod='post'>View Post</button>";
+            echo "</form>";
+            echo "</div>";
+
+            echo "</div>";
+          }
+        }
+
+        // Fetch all post tags that match search term.
+        $stmt->close();
+        $stmt = $conn->prepare("SELECT * FROM post WHERE topic Like ? ORDER BY time DESC");
+        $stmt->bind_param("s", $search);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $num_posts = $result -> num_rows;
+        echo "<div id='numPosts'>";
+        echo '<h2><u>' . $num_posts . '</u> Post Tag Result(s).</h2>';
         echo "</div>";
 
         if ($result){
@@ -85,9 +161,43 @@
             echo "</div>";
             
             echo "</div>";
-            echo "<p id='spacer'>____________________________________</p>";
           }
         }
+
+        // Fetch all post profiles that match search term.
+        $stmt->close();
+        $stmt = $conn->prepare("SELECT * FROM blogUser WHERE userName Like ?");
+        $stmt->bind_param("s", $search);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $num_posts = $result -> num_rows;
+        echo "<div id='numPosts'>";
+        echo '<h2><u>' . $num_posts . '</u> Profile Result(s).</h2>';
+        echo "</div>";
+
+        if ($result){
+          // Append this attributes, one div for each post.
+          while ($row = $result->fetch_assoc()){
+            // Fetch attributes for each post by each this user.
+            $pUserName = $row['userName'];
+            $fName = $row['firstName'];
+            $lName = $row['lastName'];
+
+            // Display post title.
+            echo "<div id='blogPost'>";
+            echo "<h2>" . $pUserName . "</h2>";
+            
+            // View post button.
+            echo "<div id='btn-holder'>";
+            echo "<form id='btn-item' action='php/viewProfile.php?user=" . $pUserName . "' method='get'>";
+            echo "<button id='btn-view' type='submit' formmethod='post'>View Profile</button>";
+            echo "</form>";
+            echo "</div>";
+            
+            echo "</div>";
+          }
+        }
+        echo "<p id='spacer'>____________________________________</p>";
 
       ?>
     </div>
