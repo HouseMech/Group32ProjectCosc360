5fbe7785840a6256cc9d3a4b5d4de4951cd592c0
Rileyccc <50608798+Rileyccc@users.noreply.github.com>
2021-04-09 13:53:06 -0700

    allow for images of type. jpg, JPG, jpeg,png

--- php/setUserImage.php
+++ php/setUserImage.php
@@ -7,18 +7,28 @@ if(!isLoggedIn()){
   exit("you musn't touch the crystals... get out of here!");
 }
 $username = $_SESSION["username"];
-// check if there is a file array, the file was uploaded through post and the name contains .jpg
+// check if there is a file array, the file was uploaded through post and the name contains .jpg, JPG, png, or jpeg
 if (count($_FILES) > 0 && is_uploaded_file($_FILES['profilePic']['tmp_name']) 
-        && (strpos($_FILES['profilePic']['name'], ".jpg") ||strpos($_FILES['profilePic']['name'], ".JPG"))) {
+        && (strpos($_FILES['profilePic']['name'], ".jpg") ||
+        strpos($_FILES['profilePic']['name'], ".JPG") ||
+        strpos($_FILES['profilePic']['name'], ".png")|| 
+        strpos($_FILES['profilePic']['name'], ".jpeg" ))) {
   $conn = createConnection();
   ## only commit after file has been successfully downloaded
   mysqli_autocommit($conn, false);  
+  // assign destintion
+  if(strpos($_FILES['profilePic']['name'], ".jpg") || strpos($_FILES['profilePic']['name'], ".JPG")){
+    $destination = "./img/profilePics/".$username.".jpg";
+  }elseif(strpos($_FILES['profilePic']['name'], ".png")){
+    $destination = "./img/profilePics/".$username.".png";
+  }else{
+    $destination = "./img/profilePics/".$username.".jpeg";
+  }
 
-  $destination = "./img/profilePics/".$username.".jpg";
   echo $destination;
   $conn->query("UPDATE bloguser SET profilePic='$destination' WHERE bloguser.userName='$username'") or die("Unable to insert image ".  mysqli_error($conn));
   
-  $destination = "../img/profilePics/".$username.".jpg";
+  $destination = ".".$destination;
   $fileToMove = $_FILES['profilePic']['tmp_name'];
   if (move_uploaded_file($fileToMove,$destination)) {
     mysqli_commit($conn);
