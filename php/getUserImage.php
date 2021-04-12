60fdf25e4856cac878d39c712beffc1ad509284f
Liam Harbec <liam.harbec@gmail.com>
2021-04-12 09:42:46 -0700

    Fixed a database naming issue

--- php/getUserImage.php
+++ php/getUserImage.php
@@ -5,11 +5,11 @@ startSession();
 # if user not logged in display defualt
 
 if(!isLoggedIn()){
-    echo "<img id='userProfilePicture' src='https://painrehabproducts.com/wp-content/uploads/2014/10/facebook-default-no-profile-pic.jpg' alt='userProfilePicture'>";   
+    echo "<img id='userProfilePicture' src='https://painrehabproducts.com/wp-content/uploads/2014/10/facebook-default-no-profile-pic.jpg' alt='userProfilePicture'>";
 }else{
     $username = $_SESSION['username'];
     $conn = createConnection();
-    $result = $conn->query("SELECT profilePic From bloguser WHERE userName='$username'");
+    $result = $conn->query("SELECT profilePic From blogUser WHERE userName='$username'");
     $row = $result->fetch_assoc();
     # if user is logged in but doesn't have a profile pic show defualt
     if(is_null($row['profilePic'])){
