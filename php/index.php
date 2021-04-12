5df2ea729bdc3ef7ffa331f339bdd72300e6ac89
Liam Harbec <liam.harbec@gmail.com>
2021-04-12 10:17:41 -0700

    Changed test username

--- php/index.php
+++ php/index.php
@@ -3,7 +3,7 @@
   startSession();
 ?>
 <?php if (isLoggedIn()): ?>
-  <a id="menuItem" href="php/viewProfile.php?user=pamalm">Profile</a>
+  <a id="menuItem" href="php/viewProfile.php?user=<?php echo $_SESSION['username']; ?>">Profile</a>
   <a id="menuItem" href="php/signOut.php">Sign Out</a>
 <?php else: ?>
   <a id="menuItem" href="pages/signIn.php">Sign In</a>
