1865646b2827f042a120b0ac749db61741e8e3df
Pamal Mangat <mangatpamal@gmail.com>
2021-04-13 22:38:18 -0700

    Admin Panel Delete Post Option Added
    - Allow admin to delete post from user table. The admin can delete the post specifically after clicking view post for a given user than delete individual posts.

---
+++ php/adminViewPost.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php include '../layouts/global_head_include.php';?>
+  <?php include_once "../php/commonFunctions.php"; ?>
+
+  <body>
+    <?php include_once '../layouts/header.php';?>
+
+  <div class="main-content">
+      <?php include '../layouts/sidebar.php';?>
+
+      <div id="center">
+
+      <?php startSession(); ?>
+      <?php if (isAdmin()): ?>
+        <?php
+          $username = $_GET['username'];
+          $adminUser = $_GET['adminUser'];
+
+          // Create connection.
+          $conn = createConnection();
+
+          // Verify that the admin is the one making the account deletion for a user.
+          $stmt = $conn->prepare("SELECT * FROM blogUser WHERE userName = ?");
+          $stmt->bind_param("s", $adminUser);
+          $stmt->execute();
+          $result = $stmt->get_result();
+          $stmt->close();
+          $row = $result->fetch_assoc();
+
+          if ($row['isAdmin'] == 1){
+            $stmt = $conn->prepare("SELECT * FROM post WHERE pUserName = ?");
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $row_cnt = $result->num_rows;
+
+          } else { exit("You must be an admin to access this page."); }
+          
+        ?>
+          <h2 id="subHead">Admin panel</h2>
+          <div class="user-amount">Viewing all post for user: <?php echo $username;?></div>
+          
+          <table class="admin-table">
+            <tr>
+              <th>Title:</th>
+              <th>Topic:</th>
+              <th>Description:</th>
+              <th>Post Time:</th>
+              <th># Likes:</th>
+              <th>Comments Allowed:</th>
+              <th>Delete Post</th>
+            </tr>
+          <?php
+            if ($row_cnt == 0){
+              echo "<tr><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td></tr>";
+            } 
+            while ($row = mysqli_fetch_assoc($result))
+            {
+              echo "<tr>";
+              echo "<td>".$row['postName']."</td>";
+              echo "<td>".$row['topic']."</td>";
+              echo "<td>".$row['description']."</td>";
+              echo "<td>".$row['time']."</td>";
+              echo "<td>".$row['likes']."</td>";
+              if ($row['allowComment'] == 1){
+                echo "<td>On</td>";
+              } else { echo "<td>Off</td>"; }
+              echo "<td>". "<a href='php/adminDeletePost.php?pid=". $row['pid'] . '&adminUser=' . $adminUser . "'>❌</a>";
+              echo "</tr>";
+            }
+            $conn->close();
+          ?>
+        </table>
+
+      <?php else: ?>
+        <h1>You do not have access to this page</h1>
+      <?php endif ?>
+      </div>
+  </div>
+  <?php include '../layouts/footer.php';?>
+  </body>
+</html>
