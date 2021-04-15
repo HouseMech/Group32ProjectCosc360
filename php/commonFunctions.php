70af6e064d2ad1b27894c608e79a51a52840e885
Pamal Mangat <mangatpamal@gmail.com>
2021-04-15 11:09:22 -0700

    Updated DB credentials with cleardb
    Database credentials in commonFunctions.php was changed to incorporate the cleardb credentials found on heroku.

--- php/commonFunctions.php
+++ php/commonFunctions.php
@@ -4,11 +4,21 @@ if(!function_exists('getConnection')){
     // create connection funcion
     function createConnection(){
         // credentials 
-        $user = 'root';
-        $pass = '';
-        $dbname = 'blog';
-        $conn = new mysqli('localhost', $user, $pass, $dbname) or die("unable to connect");
-        return $conn;
+        //$user = 'root';
+        //$pass = '';
+        //$dbname = 'blog';
+        //$conn = new mysqli('localhost', $user, $pass, $dbname) or die("unable to connect");
+        //return $conn;
+
+
+      $url = parse_url(getenv("mysql://b778a423118452:22b192a7@us-cdbr-east-03.cleardb.com/heroku_963111a3fb10ae1?reconnect=true"));
+      $server = $url["host"];
+      $username = $url["user"];
+      $password = $url["pass"];
+      $db = substr($url["path"], 1);
+      $conn = new mysqli($server, $username, $password, $db);
+      return $conn;
+
     }
 }
 
