c61a86453d794fa98edee13a51866d7ea89588ce
Rileyccc <50608798+Rileyccc@users.noreply.github.com>
2021-04-15 07:22:51 -0700

    fix relative reference issue

--- layouts/global_head_include.php
+++ layouts/global_head_include.php
@@ -2,8 +2,8 @@
   <meta charset="utf-8">
   <title>MyBlogPost</title>
   <base href = "/Group32ProjectCosc360/" />
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/header.css">
+  <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" href="./css/header.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 </head>
